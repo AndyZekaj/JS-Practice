1d85043274ce4a9e7ac7c40ba91e545e9e415a0b
Avdyl <avdyl.zekaj@dci-student.org>
2024-11-06 12:21:24 +0100

    finished first task

---
+++ task-1-1.js
@@ -0,0 +1,32 @@
+// Object: create, access, update
+// This exercise is meant to let you properly understand and practice basic operations we perform with objects, tipically:
+// creating objects
+// accessing, reading, their properties
+// updating, modifying, the values of these properties
+// Task 1
+// Given the object below, change the value of the read property from false to true. Then, output the whole object to the console.
+// const book = {
+//     title: "A Smarter Way to Learn JavaScript",
+//     author: "Mark Myers",
+//     price: 19.95,
+//     read: false
+// };
+// Expected output:
+// {
+//     title: "A Smarter Way to Learn JavaScript",
+//     author: "Mark Myers",
+//     price: 19.95,
+//     read: true
+// }
+
+const book = {
+
+    title: "A Smartern Way to Learn JavaScript",
+    author: "Mark Myers",
+    price: 19.95,
+    read: false
+};
+
+book.read = true;
+
+console.log(book);
